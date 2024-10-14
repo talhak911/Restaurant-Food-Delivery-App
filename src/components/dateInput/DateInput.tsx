0e4d247ba6ecdc576fb6c19d907451e7aa946008
Talha Sadiq <talhak911>
2024-10-14 18:36:08 +0500

    feat: make date input component

---
+++ src/components/dateInput/DateInput.tsx
@@ -0,0 +1,38 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {CustomInput} from '../customInput/CustomInput';
+import useDateInput from './useDateInput';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+const DateInput = ({
+  date,
+  setDate,
+}: {
+  date: string;
+  setDate:(date: string) => void
+}) => {
+  const {handleConfirm, hideDatePicker, showDatePicker, isDatePickerVisible} =
+    useDateInput({date,setDate});
+  return (
+    <View>
+      <TouchableOpacity onPress={showDatePicker}>
+        <CustomInput
+          value={date.split('T')[0]}
+          onChange={() => {}}
+          placeHolder="Date of Birth"
+          label="Date of Birth"
+          keyboardType="default"
+          editable={false}
+        />
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        maximumDate={new Date()}
+      />
+    </View>
+  );
+};
+
+export default DateInput;
---
+++ src/components/dateInput/useDateInput.ts
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+
+const useDateInput = ({date,setDate}:{date:string,setDate:(date: string) => void}) => {
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date: Date) => {
+    setDate(date.toISOString());
+    hideDatePicker();
+  };
+
+  return {
+    showDatePicker,
+    hideDatePicker,
+    handleConfirm,
+    isDatePickerVisible,
+  };
+};
+
+export default useDateInput;
