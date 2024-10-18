c2b5ca58fbf274875f3045eccf90664f92631ef9
Talha Sadiq <talhak911>
2024-10-18 10:04:46 +0500

    fix: modal closing gesture

--- src/components/customModal/CustomModal.tsx
+++ src/components/customModal/CustomModal.tsx
@@ -1,11 +1,10 @@
-import {Animated, Modal, StatusBar, View} from 'react-native';
+import {Animated, Modal, View} from 'react-native';
 import {useCustomModal} from './useCustomModal';
 import {styles} from './CustomModalStyles';
 import {CustomModalProps} from '../../types/types';
 
 export const CustomModal = ({visible, element, onClose}: CustomModalProps) => {
   const {panResponder, translateX} = useCustomModal({visible, onClose});
-
   return (
     <Modal
       transparent={true}
@@ -14,8 +13,12 @@ export const CustomModal = ({visible, element, onClose}: CustomModalProps) => {
       onRequestClose={onClose}>
       <View style={styles.modalOverlay}>
         <Animated.View
-          style={[styles.modalContent, {transform: [{translateX}]}]}
-          {...panResponder.panHandlers.onResponderReject}>
+          style={[styles.modalContent, {transform: [{translateX}]}]}>
+          <View
+            style={styles.closeModalContainer}
+            {...panResponder.panHandlers}>
+            <View style={styles.closeModal} />
+          </View>
           <View style={{flex: 1, width: '100%'}}>{element}</View>
         </Animated.View>
       </View>
--- src/components/customModal/CustomModalStyles.ts
+++ src/components/customModal/CustomModalStyles.ts
@@ -4,12 +4,15 @@ import {Width} from '../../utils/responsive';
 
 export const styles = StyleSheet.create({
   modalOverlay: {
+  
     flex: 1,
     justifyContent: 'flex-end',
     backgroundColor: 'rgba(0, 0, 0, 0.16)',
     flexDirection: 'row',
   },
   modalContent: {
+    position:"relative",
+    flexDirection:"row",
     backgroundColor: COLORS.orange,
     borderTopLeftRadius: 60,
     borderBottomLeftRadius: 60,
@@ -19,4 +22,17 @@ export const styles = StyleSheet.create({
     width: '80%',
     height: '100%',
   },
+  closeModalContainer:{
+    width: 60,
+    height: 100,
+    alignItems: 'center',
+    position: 'absolute',
+    left: -40,
+  },
+  closeModal:{
+    height: 90,
+    width: 6,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  }
 });
