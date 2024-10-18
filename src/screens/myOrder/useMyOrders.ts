4142cfc64798e338d4fed7eb5e85b5ebc2b9bdbf
Talha Sadiq <talhak911>
2024-10-18 20:39:27 +0500

    fix: data only refeches if the screen is focused

--- src/screens/myOrder/useMyOrders.ts
+++ src/screens/myOrder/useMyOrders.ts
@@ -1,7 +1,8 @@
-import {useEffect, useState} from 'react';
+import {useCallback, useState} from 'react';
 import {useAppDispatch, useAppSelector} from '../../hooks/useStore';
 import {fetchOrders} from '../../redux/slices/orderSlice';
 import {OrderStatus} from '../../gql/graphql';
+import {useFocusEffect} from '@react-navigation/native';
 
 export const useMyOrders = () => {
   const [isActive, setIsActive] = useState<OrderStatus>(OrderStatus.Active);
@@ -9,39 +10,43 @@ export const useMyOrders = () => {
   const [filteredOrders, setFilteredOrders] = useState(orders);
 
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(fetchOrders({}));
 
-    const intervalId = setInterval(() => {
+  useFocusEffect(
+    useCallback(() => {
       dispatch(fetchOrders({}));
-    }, 30000);
+      const intervalId = setInterval(() => {
+        dispatch(fetchOrders({}));
+      }, 30000);
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [dispatch]);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [dispatch]),
+  );
 
-  useEffect(() => {
-    if (orders) {
-      if (isActive === OrderStatus.Active) {
-        setFilteredOrders(
-          orders.filter(
-            order =>
-              order.status !== OrderStatus.Canceled &&
-              order.status !== OrderStatus.Delivered,
-          ),
-        );
-      } else if (isActive === OrderStatus.Delivered) {
-        setFilteredOrders(
-          orders.filter(order => order.status === OrderStatus.Delivered),
-        );
-      } else if (isActive === OrderStatus.Canceled) {
-        setFilteredOrders(
-          orders.filter(order => order.status === OrderStatus.Canceled),
-        );
+  useFocusEffect(
+    useCallback(() => {
+      if (orders) {
+        if (isActive === OrderStatus.Active) {
+          setFilteredOrders(
+            orders.filter(
+              order =>
+                order.status !== OrderStatus.Canceled &&
+                order.status !== OrderStatus.Delivered,
+            ),
+          );
+        } else if (isActive === OrderStatus.Delivered) {
+          setFilteredOrders(
+            orders.filter(order => order.status === OrderStatus.Delivered),
+          );
+        } else if (isActive === OrderStatus.Canceled) {
+          setFilteredOrders(
+            orders.filter(order => order.status === OrderStatus.Canceled),
+          );
+        }
       }
-    }
-  }, [orders, isActive]);
+    }, [orders, isActive]),
+  );
   const handleIsActive = (button: OrderStatus) => {
     setIsActive(button);
   };
