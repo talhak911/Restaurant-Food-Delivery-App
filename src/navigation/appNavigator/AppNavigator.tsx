25a45e9cbbe449ecc8bd420a73a23ffc1df93e11
Talha Sadiq <talhak911>
2024-10-24 09:13:51 +0500

    chore: add loader at start

--- src/navigation/appNavigator/AppNavigator.tsx
+++ src/navigation/appNavigator/AppNavigator.tsx
@@ -4,10 +4,11 @@ import DrawerNavigator from '../drawerNavigator/DrawerNavigator';
 import {RestaurantTabNavigator} from '../restaurantTabNavigator/RestaurantTabNavigator';
 import {useAppNavigator} from './useAppNavigator';
 import {CartProvider} from '../../context/cartContext/CartContext';
+import Loader from '../../components/loader/Loader';
 
 export default function AppNavigator() {
   const {initializing, user} = useAppNavigator();
-  if (initializing) return null;
+  if (initializing) return <Loader color='black'/>;
   return user?.role === 'CUSTOMER' ? (
     <NavigationContainer>
       <CartProvider>
